5e0e33fa965ae970bcd72713c6fb05a47fd9765a
siva kumar nunna <111059613+sivakumarnunna@users.noreply.github.com>
2024-02-16 11:24:05 +0000

    Update EnvReaderListener.java

--- src/main/java/src/example/listener/EnvReaderListener.java
+++ src/main/java/src/example/listener/EnvReaderListener.java
@@ -13,40 +13,8 @@ import org.testng.ITestClass;
 
 public class EnvReaderListener implements org.testng.IClassListener {
 	
-	Properties prop = new Properties();
-	private static Logger logger = LogManager.getLogger();
 	
-    public void onBeforeClass(ITestClass cls) {
-    	
-    	try {
-			prop.load(new FileInputStream("src/test/resources/config.properties"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    	
-       Set<Entry<Object,Object>> configs =  prop.entrySet();
-       
-       for(Entry<Object,Object> config : configs) {
-           String env = System.getenv(config.getKey().toString());
-           if (env != null && ! env.trim().isEmpty()) {
-        	   config.setValue(env);
-        	   
-        	   logger.info(config.getKey() +"  = "+ env);
-           }
-       }
-    	  
-       Map<String, String> parameters = cls.getXmlClass().getAllParameters();
-
-        for (Entry<String, String> parameter : parameters.entrySet()) {
-        	
-            String env = System.getenv(parameter.getKey());
-            if (env != null && ! env.trim().isEmpty()) {
-                parameter.setValue(env);
-            }
-     	   logger.info(parameter.getKey() +"  = "+ env);
-
-        }
     }
 	
  
-}
+
