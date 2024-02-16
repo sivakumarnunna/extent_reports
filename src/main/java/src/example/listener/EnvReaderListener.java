b18557a0e7f612e0776a87a4813561d42f8c991f
nunna sivakumar <nsk.arch@gmail.com>
2024-02-16 10:02:43 +0000

    message

--- src/main/java/src/example/listener/EnvReaderListener.java
+++ src/main/java/src/example/listener/EnvReaderListener.java
@@ -1,16 +1,38 @@
 package src.example.listener;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
 
 import org.testng.ITestClass;
 
 public class EnvReaderListener implements org.testng.IClassListener {
 	
+	Properties prop = new Properties();
+	
 	
     public void onBeforeClass(ITestClass cls) {
+    	
+    	try {
+			prop.load(new FileInputStream("/Users/nunnasivakumar/git/extent_reports/src/test/resources/config.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
         Map<String, String> parameters = cls.getXmlClass().getAllParameters();
-        
-        for (Map.Entry<String, String> parameter : parameters.entrySet()) {
+       Set<Entry<Object,Object>> configs =  prop.entrySet();
+       
+       for(Entry<Object,Object> config : configs) {
+           String env = System.getenv(config.getKey().toString());
+           if (env != null && ! env.trim().isEmpty()) {
+        	   config.setValue(env);
+           }
+       }
+    	           
+        for (Entry<String, String> parameter : parameters.entrySet()) {
         	
         	System.out.println(parameter.getKey());
             String env = System.getenv(parameter.getKey());
